7d4ce80c416493b4437561fac5546043dba4697e
tinonaumann <tino@naumix.de>
2020-03-12 22:38:18 +0200

    add variable check for $domain

--- nginx/rootfs/docker-entrypoint.sh
+++ nginx/rootfs/docker-entrypoint.sh
@@ -4,4 +4,9 @@ mkdir -p /var/www/html/public /var/www/logs/nginx /var/www/configs/nginx
 touch /var/www/configs/nginx/host.conf
 chown 1000:1000 /var/www/configs/nginx/ /var/www/logs/nginx/
 
+if [ -z ${domain-} ]; then
+  echo you need to define a domain
+  exit 1
+fi
+
 "$@"
