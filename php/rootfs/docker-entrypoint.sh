b5d96b26cdee1a9fb6dd9382e113e92a48c384c7
tinonaumann <tino@naumix.de>
2020-03-16 10:04:21 +0200

    update

--- php/rootfs/docker-entrypoint.sh
+++ php/rootfs/docker-entrypoint.sh
@@ -9,7 +9,9 @@ if ! [ -f "/var/www/bin/composer" ]; then
 else
   php /var/www/bin/composer self-update
 fi
-export PATH=/var/www/bin:$PATH
+cat << EOF > /etc/profile.d/modify-path.sh
+  export PATH=/var/www/bin:$PATH
+EOF
 #if ! [ -f "/var/www/bin/.composer/vendor/bin/wireshell" ]; then
 #  php -d memory_limit=-1 /var/www/bin/composer global require wireshell/wireshell -d /var/www/bin/.composer/
 #  ln -s /var/www/bin/.composer/vendor/bin/wireshell /var/www/bin/wireshell
